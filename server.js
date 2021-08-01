21db80629bf067d03fe072cc5a4e1c6d7dc16023
Vincent T <vyncode@gmail.com>
2021-07-31 20:40:36 -0400

    attempting ag route

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Workout = require("./models/models");
-path = require("path")
+const path = require("path")
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    process.env.MONGODB_URI || 'mongodb://localhost/3000',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -12,3 +13,84 @@ mongoose.connect(
         useFindAndModify: false
     }
 );
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static(path.join(__dirname, "public")));
+
+// main home routes
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(`${__dirname}/public/exercise.html`)
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(`${__dirname}/public/stats.html`)
+});
+
+// api routess 
+app.get("/api/workouts", async (req, res) => {
+    try {
+        const userData = await Workout.aggregate([
+            {
+                $addFields: {
+                    "totalDuration.totalDuration": {
+                        $sum: "$exercises.duration"
+                    }
+                }
+            }]);
+
+        if (!userData.length) {
+            res.status(404).json({
+                message: "No Workouts found in the database"
+            });
+        } else {
+            res.status(200).json(userData);
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// create 
+app.post("/api/workouts", async (req, res) => {
+    try {
+        const createUserData = await Workout.create(req.body);
+        console.log(createUserData);
+        res.status(200).json(createUserData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// update
+app.put("/api/workouts/:id", async (req, res) => {
+    try {
+        console.log(req.body);
+        const updateData = await Workout.findByIdAndUpdate({
+            _id: req.params.id
+        }, {
+            $push: {
+                exercises: req.body
+            }
+        });
+        console.log(updateData);
+        res.status(200).json(req.body);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// range
+
+// not sure yet need to finish later
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
